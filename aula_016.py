a1d231a14fb07ad05e9c0275042463c4c39bc024
Helton Barbosa <heltonbs@gmail.com>
2023-03-25 07:31:35 -0300

    Introdução aos blocos de código + if / elif / else (condicionais)

---
+++ aula_016.py
@@ -0,0 +1,11 @@
+# if / elif       / else
+# se / senão se   / senão
+
+entrada = input('(1) - Entrar\n(2) - Sair\nEscolha: ')
+
+if entrada == '1':
+    print('Você entrou no sistema.')
+elif entrada == '2':
+    print('Você saiu do sistema.')
+else:
+    print('Não compreendi a sua escolha.')
