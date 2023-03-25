84ebd87def8df328b308394803c07c5d7186ea97
Helton Barbosa <heltonbs@gmail.com>
2023-03-24 23:40:45 -0300

    Formatação de Strings com o método format()

--- aula_014.py
+++ aula_014.py
@@ -1,6 +1,16 @@
 a = 'A'
 b = 'B'
 c = 1.1
-formato = ''
 
+q = 'HBSF'
+w = 'Rose'
+e = 1500.2
+
+formato = 'a = {} - b = {} - c = {:.2f}'.format(a, b, c)
+formato_2 = 'e = {nome3:,.2f} - w = {nome2} - q = {nome1}'.format(
+   nome1 = q,
+   nome2 = w,
+   nome3 = e
+)
 print(formato)
+print(formato_2)
