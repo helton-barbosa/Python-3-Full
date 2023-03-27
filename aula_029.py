b08dd3d333d509866d537fe23cc14e040d4bcb44
Helton Barbosa <heltonbs@gmail.com>
2023-03-27 07:08:15 -0300

    Intro try e except pra capturar erros (exceptions)

--- aula_029.py
+++ aula_029.py
@@ -1,15 +1,5 @@
-nome = input('Digite seu nome: ')
-idade = input('Digite sua idade: ')
-
-if nome and idade:
-    print(f'Seu nome é {nome}.')
-    print(f'Seu nome invertido é {nome[::-1]}.')
-    if ' ' in nome:
-      print(f'Seu nome contém espaços.')
-    else:
-      print(f'Seu nome não contém espaços.')
-    print(f'Seu nome contém {len(nome)} letras.')
-    print(f'A primeira letra do seu nome é {nome[0]}.')
-    print(f'A última letra do seu nome é {nome[len(nome)-1:]}')
-else:
-    print('Desculpe, você deixou campos vazios.')
+"""
+Introdução ao try/except
+try -> tentar executar o código
+except -> ocorreu algum erro ao tentar executar
+"""
