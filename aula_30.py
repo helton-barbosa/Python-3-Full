f6072fe1fe4bd4cac2248a5eae291b56b5338fd3
Helton Barbosa <heltonbs@gmail.com>
2023-03-27 20:08:50 -0300

    Variáveis, constantes e comp. de código - Pt1

--- aula_30.py
+++ aula_30.py
@@ -1,7 +1,16 @@
 """
 CONSTANTE = "Variáveis" que não vão mudar
+            Python não possui um tipo de dado "constante" em si,
+            mas é uma prática comum nomear variáveis que armazenam
+            valores constantes em letras maiúsculas para indicar
+            que elas não devem ser alteradas. Isso é conhecido como
+            "convenção de nomenclatura de constantes".
 Muitas condições no mesmo if(ruim)
-    <- Contagem de complexidade (ruim)
+    Quanto mais condições em um IF, mais difícil será para 
+    entender o código.
+<- Contagem de complexidade (ruim)
+    Quanto mais houver recuo de código, mais complexo pode ficar
+    seu código. Quanto mais evitar, melhor.
 """
 
 velocidade = 61 # velocidade atual do carro
