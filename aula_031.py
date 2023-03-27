2cfccbe3f49a8bb84d47924964eb3e08fc4757aa
Helton Barbosa <heltonbs@gmail.com>
2023-03-27 20:42:02 -0300

    id - A identidade do valor que está na memória

--- aula_031.py
+++ aula_031.py
@@ -4,3 +4,15 @@ None = Não valor
 is e is not = é ou não é (tipo, valor, identidade)
 id = identidade
 """
+
+v1 = 'a'
+v2 = 'b'
+print(id(v1))
+print(id(v2))
+
+# confição = False
+
+# if condicao:
+#   print('Faça algo)
+# else:
+#   print('Não faça algo)
