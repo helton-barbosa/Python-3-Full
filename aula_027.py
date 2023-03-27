f7cc5cfd3a42e05e461f069f21f9ca43caa71595
Helton Barbosa <heltonbs@gmail.com>
2023-03-26 21:53:35 -0300

    Fatiamento de strings e a função len

---
+++ aula_027.py
@@ -0,0 +1,17 @@
+"""
+Fatiamento de strings
+ 012345678
+ Olá mundo
+-987654321
+Fatiamento [i:f:p] [::]
+            i = início
+            f = fim
+            p = passo
+Obs.: a função len retorna a qtd
+de caracteres da str
+"""
+variavel = 'Olá mundo'
+# print(variavel[2:7])
+# print(variavel[:3])
+# print(variavel[4:])
+print(variavel[::-1])
