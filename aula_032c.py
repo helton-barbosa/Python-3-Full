dca21b2b9e47df9c9e620490e7f35dd8dfc78d4c
Helton Barbosa <heltonbs@gmail.com>
2023-03-28 09:47:33 -0300

    Exercício 3

---
+++ aula_032c.py
@@ -0,0 +1,7 @@
+"""
+Faça um programa que peça o primeiro nome do usuário.
+Se o nome tiver 4 letras ou menos, escreva "Seu nome é curto"
+Se o nome tiver entre 5 e 6 letras, escreva "Seu nome é normal"
+Se o nome tiver mais que 6 letras, escreva "Seu nome é muito grande".
+"""
+
