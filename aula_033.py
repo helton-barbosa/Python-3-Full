ecfb4d89a55f94e1698c46c5d43c041ffef66e40
Helton Barbosa <heltonbs@gmail.com>
2023-04-06 18:49:02 -0300

    built-in, tipo imutáveis, métodos de string

---
+++ aula_033.py
@@ -0,0 +1,9 @@
+"""
+https://docs.python.org/pt-br/3/library/stdtypes.html
+Imutáveis que já vimos: str, int, float, bool
+"""
+
+string = 'helton barbosa santos ferreira'
+# string[3] = 'Z'
+print(string)
+print(string.title())
