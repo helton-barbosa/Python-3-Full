491c3a174fe4340b6ab3e7a49cd9412938cab4db
Helton Barbosa <heltonbs@gmail.com>
2023-04-07 22:36:03 -0300

    while e break - Estrutura de repetição (Parte 1)

--- aula_034.py
+++ aula_034.py
@@ -2,4 +2,14 @@
 Repetições:
 while (enquanto)
 Executa uma ação enquanto uma condição for verdadeira
+Loop Infinito - Quando um código não tem fim
 """
+condicao = True
+
+while condicao:
+    nome = input('Qual o seu nome: ')
+    print(f'Seu nome é {nome}')
+    if nome == 'sair':
+        break
+
+print('Fim')
