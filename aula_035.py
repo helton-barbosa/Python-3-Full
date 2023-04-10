34171fad413c0fe56277899a6c2cfbc0a9e8df2e
Helton Barbosa <heltonbs@gmail.com>
2023-04-10 06:16:32 -0300

    while - Condição em detalhes

--- aula_035.py
+++ aula_035.py
@@ -4,3 +4,10 @@ while (enquanto)
 Executa uma ação enquanto uma condição for verdadeira
 Loop Infinito - Quando um código não tem fim
 """
+contador = 0
+
+while contador < 10:
+    contador += 1
+    print(contador)
+
+print('Acabou')
