a486f9e0ac257ec6dd73425a4f2a0f414591aca9
Helton Barbosa <heltonbs@gmail.com>
2023-04-10 06:47:20 -0300

    while + continue - pulando alguma repetição

--- aula_037.py
+++ aula_037.py
@@ -5,3 +5,12 @@ Executa uma ação enquanto a condição for verdadeira
 Loop infinito -> Quando um código não tem fim
 """
 
+contador = 0
+
+while contador <= 30:
+    contador += 1
+    if contador % 2 == 0:
+        print(f'O número {contador} é PAR.')
+    else:
+        print('Número ÍMPAR.')
+        continue
