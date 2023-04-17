7d9056437ab209d17b76600fa885f68523a1f8c5
Helton Barbosa <heltonbs@gmail.com>
2023-04-17 17:15:08 -0300

    Resolução do exercício

--- aula_039.py
+++ aula_039.py
@@ -2,3 +2,16 @@
 Exercício:
 Interando strings com while
 """
+
+# *H*e*l*t*o*n
+
+nome = input('Digite o nome que deseja iterar: ')
+tamanho_nome = len(nome)
+iterador = 0
+novo_nome = ''
+
+while iterador < tamanho_nome:
+    novo_nome += '*' + nome[iterador]
+    iterador += 1
+
+print(f'O nome modificado ficará assim: {novo_nome}')
