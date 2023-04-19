2a93a55b28aecd40766761c5bf84f3ecf427343a
Helton Barbosa <heltonbs@gmail.com>
2023-04-19 10:03:47 -0300

    Calcuradora utilizando While - Pt 1

--- aula_040.py
+++ aula_040.py
@@ -1,3 +1,14 @@
 """
 Calculadora com While
 """
+
+while True:
+    numero_01 = int(input('Entre com o primeiro número: '))
+    numero_02 = int(input('Entre com o segundo número: '))
+
+    sair = input('Encerrar? [s]im: ')
+    sair = sair.lower()
+    sair = sair.startswith('s')
+
+    if sair is True:
+        break
