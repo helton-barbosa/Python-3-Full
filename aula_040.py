5caaba71d6ef68270c65fd05abe74addbaae8916
Helton Barbosa <heltonbs@gmail.com>
2023-04-20 09:36:36 -0300

    Exercício guiado - Calculadora - Parte 2

--- aula_040.py
+++ aula_040.py
@@ -3,8 +3,29 @@ Calculadora com While
 """
 
 while True:
-    numero_01 = int(input('Entre com o primeiro número: '))
-    numero_02 = int(input('Entre com o segundo número: '))
+    numero_01 = input('Entre com o primeiro número: ')
+    numero_02 = input('Entre com o segundo número: ')
+    operador = input('Escolha o operador [+ - * /]: ')
+
+    numeros_validos = None
+
+    try:
+        num_1_float = float(numero_01)
+        num_2_float = float(numero_02)
+        numeros_validos = True
+    except:
+        print('Um ou ambos dos números são inválidos.')
+        continue
+
+    operadores_permitidos = '+-/*'
+
+    if operador not in operadores_permitidos:
+        print('Operador inválido.')
+        continue
+
+    if len(operador) > 1:
+        print('Digite apenas um operador.')
+        continue
 
     sair = input('Encerrar? [s]im: ')
     sair = sair.lower()
