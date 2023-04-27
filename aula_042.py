1d7677d12314f75d3df20325c3da36838601e246
Helton Barbosa <heltonbs@gmail.com>
2023-04-27 12:15:04 -0300

    Debugando o código

--- aula_042.py
+++ aula_042.py
@@ -1,22 +1,20 @@
-frase = 'O Python é uma linguagem de programação '\
-        'multiparadiga. '\
-        'Python foi criado por Guido van Rossum.'
-
+frase = 'Helton Barbosa'
+tamanho_frase = len(frase)
 i = 0
-qtd_apareceu_mais_vezes = 0
-letra_apareceu_mais_vezes = ''
-while i < len(frase):
+qtd_mais_vezes = 0
+letra_mais_vezes = ''
+while i < tamanho_frase:
     letra_atual = frase[i]
     if letra_atual == ' ':
         i += 1
         continue
 
-    qtd_apareceu_mais_vezes_atual = frase.count(letra_atual)
+    qtd_atual = frase.count(letra_atual)
 
-    if qtd_apareceu_mais_vezes < qtd_apareceu_mais_vezes_atual:
-        qtd_apareceu_mais_vezes = qtd_apareceu_mais_vezes_atual
-        letra_apareceu_mais_vezes = letra_atual
+    if qtd_mais_vezes < qtd_atual:
+        qtd_mais_vezes = qtd_atual
+        letra_mais_vezes = letra_atual
 
     i += 1
 
-print(f'A letra que apareceu mais vezes foi "{letra_apareceu_mais_vezes}" que apareceu {qtd_apareceu_mais_vezes}x')
+print(f'A letra que apareceu mais vezes foi "{letra_mais_vezes}" que apareceu {qtd_mais_vezes}x')
