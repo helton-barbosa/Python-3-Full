564f6e736c89227a94a58defe5082137e878833b
Helton Barbosa <heltonbs@gmail.com>
2023-04-29 20:43:25 -0300

    Cuidados com tipos de dados mutáveis - list e copy

--- aula_048d.py
+++ aula_048d.py
@@ -3,3 +3,10 @@ Cuidados com dados mutáveis
 = - copiado o valor (imutáveis)
 = - aponta para o mesmo valor na memória (mutável)
 """
+
+lista_a = ['Helton', 'Rose', True, 100]
+lista_b = lista_a.copy()
+
+lista_a[0] = 'Casa'
+print(f'Lista A {lista_a}')
+print(f'Lista B {lista_b}')
