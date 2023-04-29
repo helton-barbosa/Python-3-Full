8d3764b8d67b6c74b2d19b1c109b2773b62f8ac1
Helton Barbosa <heltonbs@gmail.com>
2023-04-28 21:52:24 -0300

    Tipo list - Introdução às listas mutáveis

---
+++ aula_048.py
@@ -0,0 +1,7 @@
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis: append, insert, pop, del, clear, extend, +
+"""
