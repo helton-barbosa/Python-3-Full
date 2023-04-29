af85054741a159369eed82c4569a16451b3af65c
Helton Barbosa <heltonbs@gmail.com>
2023-04-29 20:23:36 -0300

    Concatenando e estendendo listas em Python

---
+++ aula_048c.py
@@ -0,0 +1,19 @@
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis:
+    append - Adiciona um item ao final da lista. Qualquer dado.
+    insert - Adiciona um item na lista no índice escolhido
+    pop - Remove do final ou do índice escolhido
+    del - Apaga um índice
+    clear - Limpa a lista
+    Extend - Estende a lista
+    + - Concatena as listas
+Create  Read    Update  Delete
+Criar   Ler     Alterar Apagar = lista[i] (CRUD)
+"""
+
+lista_a = []
+lista_b = []
