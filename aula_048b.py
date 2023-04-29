ad0b5361063214b4da94fd4e041d168dd9f692b3
Helton Barbosa <heltonbs@gmail.com>
2023-04-29 20:20:13 -0300

    Add itens em qualquer índice da lista com insert

--- aula_048b.py
+++ aula_048b.py
@@ -4,7 +4,7 @@ Tipo list - Mutável
 Suporta vários valores de qualquer tipo
 Conhecimentos reutilizáveis - índices e fatiamento
 Métodos úteis:
-    append - Adiciona um item ao final da lista
+    append - Adiciona um item ao final da lista. Qualquer dado.
     insert - Adiciona um item na lista no índice escolhido
     pop - Remove do final ou do índice escolhido
     del - Apaga um índice
@@ -16,3 +16,21 @@ Criar   Ler     Alterar Apagar = lista[i] (CRUD)
 """
 
 lista = [10, 20, 30, 40]
+print(f'Lista original -> {lista}')
+
+append_01 = 'Python'
+append_02 = False
+lista.append(append_01)
+lista.append(append_02)
+print(f'Adicionando {append_01} e {append_02} à lista. Lista atualizada -> {lista}')
+
+pop_01 = lista.pop(5)
+print(f'Removendo {pop_01} da lista. Lista atualizada -> {lista}')
+
+del lista[-1]
+print(f'Deletando o último item da lista. Lista atualizada -> {lista}')
+
+indice_add = 0
+item_add = 'Flamengo'
+lista.insert(indice_add, item_add)
+print(f'Adicionando {item_add} no índice {indice_add} da lista. Lista atualizada-> {lista}')
