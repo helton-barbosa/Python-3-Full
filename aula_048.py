b7f55029df8abf1196c98375294cf8ee69989338
Helton Barbosa <heltonbs@gmail.com>
2023-04-29 19:02:52 -0300

    Alterando uma lista com índices, del, append e pop

--- aula_048.py
+++ aula_048.py
@@ -5,3 +5,10 @@ Suporta vários valores de qualquer tipo
 Conhecimentos reutilizáveis - índices e fatiamento
 Métodos úteis: append, insert, pop, del, clear, extend, +
 """
+#         01234
+#        -54321
+string = 'ABCDE' # 5 caracteres
+# print(lista, type(lista))
+lista = [123, True, 'Helton', 5.5]
+lista[2] = 'Helton Barbosa'
+print(lista[-2], type(lista[2]))
