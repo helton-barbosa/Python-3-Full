3e05b881bd988dd41f9acb97eac040093862a3d9
Helton Barbosa <heltonbs@gmail.com>
2023-05-01 16:31:24 -0300

    enumerate para enumerar valores de iteráveis

---
+++ aula_053.py
@@ -0,0 +1,8 @@
+"""
+Enumerate - enumera iteráveis (índices)
+"""
+
+lista_frutas = ['banana', 'maracujá', 'manga', 'tamarindo', 'limão', 'laranja', 'umbu', 'caju', 'framboesa']
+
+for indice, fruta in enumerate(lista_frutas):
+    print(f'Índice {indice} - Fruta "{fruta}"')
