78f2205318dcc813108653239f4bc64cff38a128
Helton Barbosa <heltonbs@gmail.com>
2023-05-08 10:44:00 -0300

    Refatorando o código

--- seção_03/aula_061c.py
+++ seção_03/aula_061c.py
@@ -1,4 +1,5 @@
-cpf = '746.824.890-70'
+cpf = '062.922.131-61'
+cpf_limpo = cpf.replace('.', '').replace('-', '')
 numeros_1 = []
 numeros_2 = []
 pri_digito = int(cpf[-2])
@@ -8,14 +9,12 @@ contador_regressivo_2 = 11
 resultado_1 = 0
 resultado_2 = 0
 
-for i, digito in enumerate(cpf):
-    if digito == '.':
-        continue
-    if digito == '-':
-        break
-
+for digito in cpf_limpo:
     numeros_1.append(int(digito) * contador_regressivo_1)
     contador_regressivo_1 -= 1
+
+    if contador_regressivo_1 == 1:
+        break
 
 for digito in numeros_1:
     resultado_1 += digito
@@ -23,16 +22,16 @@ for digito in numeros_1:
 resultado_1 *= 10
 resultado_1 %= 11
 
-primeiro_digito = f'1º dígito é 0.' if resultado_1 > 9 else f'1º dígito é {resultado_1}.'
+if resultado_1 > 9:
+    resultado_1 = 0
+else:
+    resultado_1 = resultado_1
 
-for i, digito in enumerate(cpf):
-    if digito == '.' or digito == '-':
-        continue
-
+for digito in cpf_limpo:
     numeros_2.append(int(digito) * contador_regressivo_2)
     contador_regressivo_2 -= 1
 
-    if i == 12:
+    if contador_regressivo_2 == 1:
         break
 
 for digito in numeros_2:
@@ -41,7 +40,10 @@ for digito in numeros_2:
 resultado_2 *= 10
 resultado_2 %= 11
 
-segundo_digito = f'2º dígito é 0.' if resultado_2 > 9 else f'2º dígito é {resultado_2}.'
+if resultado_2 > 9:
+    resultado_2 = 0
+else:
+    resultado_2 = resultado_2
 
 if pri_digito == resultado_1 and seg_digito == resultado_2:
     print(f'O cpf {cpf} é válido!')
