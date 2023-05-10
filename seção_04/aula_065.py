b175dfb7a26e31896a7c80a598db7dbc5e706d1d
Helton Barbosa <heltonbs@gmail.com>
2023-05-10 05:27:30 -0300

    Introdução às funções - def define uma função

--- seção_04/aula_065.py
+++ seção_04/aula_065.py
@@ -6,3 +6,22 @@ Elas podem receber valores para parâmetros (argumentos)
 e retornar um valor específico.
 Por padrão, funções Python retornam None (nada).
 """
+
+
+def imprimir():
+    print('Funções em Python')
+
+
+def somar(numero_1, numero_2):
+    soma = numero_1 + numero_2
+    print(f'A soma de {numero_1} + {numero_2} = {soma}')
+
+
+def saudacao(nome='Sem nome'):
+    print(f'Olá, {nome}!')
+
+
+imprimir()
+somar(15, 45)
+saudacao('Helton Barbosa')
+saudacao()
