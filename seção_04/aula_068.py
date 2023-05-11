729d04db64541f2579f37649537e86d5d34e3d85
Helton Barbosa <heltonbs@gmail.com>
2023-05-11 16:14:07 -0300

    (Pt 1) Escopo de funções e módulos em Python + global

---
+++ seção_04/aula_068.py
@@ -0,0 +1,29 @@
+"""
+Escopo de funções em Python
+Escopo significa o local onde aquele código pode atingir
+Existem o escopo global e local
+O escopo global é o escopo onde todo o código é alcançável
+O escopo local é o escopo onde apenas nomes do mesmo local podem ser alcançados
+"""
+
+x = 1
+
+
+def escopo():
+    global x
+    x = 10
+    print(x)
+
+    def outra_funcao():
+        global x
+        x = 30
+        y = 20
+        print(x, y)
+
+    outra_funcao()
+    print(x)
+
+
+print(x)
+escopo()
+print(x)
