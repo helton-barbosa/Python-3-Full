4a7ebb8c12b401576bcf16d75bbeecf8b04cab3f
Helton Barbosa <heltonbs@gmail.com>
2023-05-13 22:10:33 -0300

    Pt 2 - *args para qtd de argumentos não nomeados

--- seção_04/aula_071.py
+++ seção_04/aula_071.py
@@ -3,15 +3,26 @@ args - Argumentos não nomeados
 * - *args (empacotamento e desempacotamento)
 """
 # Lembre-se de desempacotamento
-x, y, *resto = 1, 2, 3, 4
-print(x, y, resto)
+# x, y, *resto = 1, 2, 3, 4
+# print(x, y, resto)
+
+print('Uma forma de somar...')
 
 
 def soma(*args):
     total = 0
     for numero in args:
         total += numero
-        print(numero, total)
+
+    return total
 
 
-soma(1, 2, 3, 4, 5, 6)
+retorno = soma(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+print(retorno)
+
+print('Outra forma de somar...')
+numeros = 10, 20, 30, 40, 50, 1, 2, 3, 4, 5
+print(numeros, type(numeros))
+a = sum(numeros)
+print(a)
+print(f'Os números são {numeros} e a soma deles é {sum(numeros)}.')
