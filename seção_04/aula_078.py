161c4e2a6857f69ee7a8bbdad5160f31959eb798
Helton Barbosa <heltonbs@gmail.com>
2023-05-27 17:27:39 -0300

    Introdução ao tipo SET no Python

---
+++ seção_04/aula_078.py
@@ -0,0 +1,20 @@
+# Sets - Conjuntos em Python (tipo set)
+# Conjuntos são ensinados na matemática
+# https://brasilescola.uol.com.br/matematica/conjunto.htm
+# Representados graficamente pelo diagrama de Venn
+# Sets em Python são mutáveis, porém, aceitam apenas tipos imutáveis como valor interno.
+
+# Criando um set
+# set(iteravel) ou {1, 2, 3}
+
+s1 = set('Helton')
+# s1 = {'Helton', 1, 2, 3}
+print(s1, type(s1))
+
+# Set são eficientes para remover valores duplicados de iteráveis
+# - Eles não tem índexes
+# - Eles não garantem ordem
+# - Eles são iteráveis (for, in, not in)
+
+# Métodos úteis:
+# add, update, clear, dicard
