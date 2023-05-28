c8d0848a68350811606cbc239c2853f163b26cfe
Helton Barbosa <heltonbs@gmail.com>
2023-05-28 15:31:43 -0300

    Introdução à função lambda + list.sort e sorted

---
+++ seção_04/aula_081.py
@@ -0,0 +1,15 @@
+"""
+Função lambda em Python
+
+A função lambda é uma função como outra qualquer em Python.
+Porém, são funções anônimas que contém apenas uma linha.
+Ou seja, tudo deve ser contido dentro de uma única expressão.
+
+lista = [
+    {'Nome': 'Helton', 'Sobrenome': 'Barbosa'},
+    {'Nome': 'Rosilene', 'Sobrenome': 'Xavier'},
+    {'Nome': 'João Victor', 'Sobrenome': 'Barbosa'},
+    {'Nome': 'Rebecca', 'Sobrenome': 'Ferreira'},
+    {'Nome': 'Alailda', 'Sobrenome': 'Santos'},
+]
+"""
