b6cfb99ea2d5626765973bd13b836ae2b1309a48
Helton Barbosa <heltonbs@gmail.com>
2023-05-28 15:52:56 -0300

    Introdução à função lambda + list.sort e sorted

--- seção_04/aula_081.py
+++ seção_04/aula_081.py
@@ -13,3 +13,45 @@ lista = [
     {'Nome': 'Alailda', 'Sobrenome': 'Santos'},
 ]
 """
+
+lista_de_numeros_1 = [3, 5, 7, 23, 43, 54, 12, 23, 34, 54, 87, 0, 98, 9, 4]
+lista_de_numeros_1.sort()
+lista_de_numeros_2 = sorted(lista_de_numeros_1)
+print(lista_de_numeros_1)
+print(lista_de_numeros_2)
+
+lista = [
+    {'Nome': 'Helton', 'Sobrenome': 'Barbosa', 'Idade': 37},
+    {'Nome': 'Rosilene', 'Sobrenome': 'Xavier', 'Idade': 40},
+    {'Nome': 'João Victor', 'Sobrenome': 'Barbosa', 'Idade': 12},
+    {'Nome': 'Rebecca', 'Sobrenome': 'Ferreira', 'Idade': 11},
+    {'Nome': 'Alailda', 'Sobrenome': 'Santos', 'Idade': 63},
+]
+
+
+# def ordena(item):
+#     return item['Sobrenome']
+
+# lista.sort(key=ordena)
+
+# lista.sort(key=lambda item: item['Idade'])
+
+# for item in lista:
+#     print(item)
+
+def exibir(lista):
+    for item in lista:
+        print(f'{item}')
+    print()
+
+
+lista_01 = sorted(lista, key=lambda item: item['Nome'])
+lista_02 = sorted(lista, key=lambda item: item['Sobrenome'])
+lista_03 = sorted(lista, key=lambda item: item['Idade'])
+
+print('Exibir a lista ordenada por Nome')
+exibir(lista_01)
+print('Exibir a lista ordenada por Sobrenome')
+exibir(lista_02)
+print('Exibir a lista ordenada por Idade')
+exibir(lista_03)
