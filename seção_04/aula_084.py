7a888d0b955c572712e96f34ee1a43ed554aa05c
Helton Barbosa <helton.bs@defensoria.to.def.br>
2023-06-12 14:23:44 -0300

    Revisando e exercitando a aula

--- seção_04/aula_084.py
+++ seção_04/aula_084.py
@@ -3,12 +3,20 @@ List comprehension em Python
 List comprehension é uma forma rápida para criar listas a partir de iteráveis
 """
 lista = []
-for numero in range(21):
+for numero in range(16):
     lista.append(numero)
-# print(lista)
+print(lista)
 
 lista = [
     numero * numero
     for numero in range(16)
 ]
 print(lista)
+print()
+
+frutas = [
+    fruta.capitalize()
+    for fruta in ['abacaxi', 'maçã', 'banana', 'morango', 'sapoti']
+]
+
+print(frutas)
