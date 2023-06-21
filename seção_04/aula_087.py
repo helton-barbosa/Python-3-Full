80962983dcd1419499f7977ab290695fb29c72e1
Helton Barbosa <helton.bs@defensoria.to.def.br>
2023-06-21 17:14:03 -0300

    Uso do isinstance para verificar o tipo de objeto

--- seção_04/aula_087.py
+++ seção_04/aula_087.py
@@ -2,4 +2,16 @@
 # isinstance serve para verificar se o objeto é de um determinado tipo
 """
 
-lista = ['a', 1, 1.1, True, [0, 1, 2], (1, 2), {0, 1}, {'Nome': 'Helton'}]
+lista = ['a', 'h', 1, 1.1, True, [0, 1, 2], (1, 2), {0, 1}, {'Nome': 'Helton'}]
+
+for item in lista:
+    if isinstance(item, set):
+        item.add(7)
+        print(f'Conjuntos SET\n{item}')
+    elif isinstance(item, str):
+        print(f'String\n{item.upper()}')
+
+    elif isinstance(item, (int, float)):
+        print(f'Numéricos (int ou float)\n{item * 2.5}')
+    else:
+        print(f'Outros Tipos\n{item}')
